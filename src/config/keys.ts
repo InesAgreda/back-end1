012e452e8a0cde440e0194a20aecb2aa3f32e43a
InesAgreda <56413828+InesAgreda@users.noreply.github.com>
2021-08-20 15:19:52 -0500

    Update keys.ts
    Actualizar datos

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,9 +1,9 @@
 export default{
     database: {
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '',
+        host: 'db-mysql-nyc3-72867-do-user-9675080-0.b.db.ondigitalocean.com',
+        port: 25060,
+        user: 'doadmin',
+        password: 'a0rqv8yb6wu9kwm7',
         database: 'db_desarrollo'
     }
 }
